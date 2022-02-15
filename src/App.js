1569dc171ebd45461fac7152fce22434fb989524
JorgeVargas <javargas216@unicauca.edu.co>
2022-02-15 17:15:14 -0500

    TodoList completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialTodos = [
+    {
+        id: 1,
+        title: 'Todo #1',
+        description: 'Desc del Todo #1',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Todo #2',
+        description: 'Desc del Todo #2',
+        completed: true
+    }
+]
+
+const App = () => {
+
+    const [todos, setTodos] = useState(initialTodos);
+
+    const todoDelete = (todoId) =>{
+        const changedTodos = todos.filter(todo => todo.id !== todoId);
+        setTodos(changedTodos);
+    }
+
+    const todoToogleCompleted = (todoId) => {
+        const changedTodos = todos.map(todo => todo.id === todoId ? {...todo, completed: !todo.completed} : todo)
+        setTodos(changedTodos);
+    }
+
+    return (
+        <div className='container mt-4'>
+            <div className='row'>
+                <div className='col-8'>
+                    <TodoList 
+                        todos={todos}
+                        todoDelete={todoDelete}
+                        todoToogleCompleted={todoToogleCompleted}
+                    />
+                </div>
+                <div className='col-4'>
+                    <TodoForm/>
+                </div>
+            </div>
+
+        </div>
+
+    );
 }
 
 export default App;
